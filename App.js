ea626c68e3dc9dfb088215d9409bd969789ae5c3
joaohenriquepda <joaohenrique.p.almeida@gmail.com>
2017-12-27 20:21:13 -0200

    Adding layout

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, TextInput } from 'react-native';
 import { Provider } from 'react-redux';
 
 import store from './src/store/';
-import ToDo from './src/components/ToDoList'
+import ToDoList  from './src/components/ToDoList'
 
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
   render() {
     return (
       <Provider store={store}>
         <View style={styles.container}>
-          <Text>Open App.js tart working on your app!</Text>
-          <Text>Mudanças</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
+          <ToDoList/>
         </View>
       </Provider>
     );
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
-import { Button } from 'react-native'
+import { AppRegistry, StyleSheet, Text, View, TextInput } from 'react-native';
+import { Button,FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
 
-export default class ToDoList extends Component {
 
-  onPressButton= ()=>{
-    alert("ddd")
+ export default class ToDoList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { text: '',
+      error:''
+    };
   }
+
+  press = () =>{
+    alert("PRESS")
+  }
+
+  errorHandler(code, message) {
+  this.setState({
+    error: !code ? null :
+      {
+        code,
+        message,
+      }
+  })
+}
+
 
   render(){
     return(
+    <View>
+      <FormLabel>Name ToDo</FormLabel>
+      <TextInput
+        shake={this.state.error}
+        onChangeText={(text) => this.setState({text})}
+        value={this.state.text}
+      />
 
-      <View>
-            <Buttom
-            onPress={onPressButton}
-            title="Add"
-            />
-      </View>
+        <Button
+          raised
+          icon={{name: 'add'}}
+          onPress={()=>{
+            this.press()
+          }}
+          title="Add new ToDo"
+          />
+    </View>
     );
   }
 }
